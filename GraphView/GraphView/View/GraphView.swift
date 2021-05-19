86583ac5a7da82a847a8c680e610cd5e2c3fbfb9
europeec <l.europeec@ya.ru>
2021-05-19 12:35:41 +0300

    popover

--- GraphView/GraphView/View/GraphView.swift
+++ GraphView/GraphView/View/GraphView.swift
@@ -9,27 +9,39 @@ import SwiftUI
 
 struct GraphView: View {
     let data: Graph
-    
+    @State var popOverPresented = false
+    @State var isDisabled = false
+    @State var text = "hello"
     var body: some View {
-        NavigationLinkt(
-            destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/,
-            label: {
-                ZStack {
-                    ZStack {
-                        Circle()
-                            .stroke(lineWidth: 6)
-                        Text(data.description)
-                            .fontWeight(.bold)
-                            .font(.title)
-                            .minimumScaleFactor(0.7)
-                            .lineLimit(3)
-                            .truncationMode(.middle)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 150, height: 150)
-                        
-                    }.frame(width: 200, height: 200)
-                }
-            })
+        Button(action: {
+            self.popOverPresented = true
+        }, label: {
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 6)
+                Text(text)
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .minimumScaleFactor(0.7)
+                    .lineLimit(3)
+                    .truncationMode(.middle)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 150, height: 150)
+                
+            }.frame(width: 200, height: 200)
+        }).popover(isPresented: $popOverPresented) {
+            Text("???")
+                .onAppear(perform: {
+                    isDisabled = true
+                })
+                .onDisappear(perform: {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        isDisabled = false
+                    }
+                })
+        }
+        .disabled(isDisabled)
+        
     }
 }
 
